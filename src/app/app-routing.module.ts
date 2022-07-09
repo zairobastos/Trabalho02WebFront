0a67e9678930482a8006804a156aa71956642d7b
Zairo Bastos <zairo_vianahd@hotmail.com>
2022-07-09 18:24:30 -0300

    Falta somente a tela de conversa

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,30 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { HeaderComponent } from "./components/template/header/header.component";
+import { HomeComponent } from "./components/template/home/home.component";
+import { CadastroComponent } from "./components/template/cadastro/cadastro.component";
+import { AdicionarComponent } from "./components/template/adicionar/adicionar.component";
+const routes: Routes = [
+  {
+    path: "",
+    component: HeaderComponent,
+  },
+  {
+    path: "home",
+    component: HomeComponent,
+  },
+  {
+    path: "singup",
+    component: CadastroComponent,
+  },
+  {
+    path: "adicionar",
+    component: AdicionarComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,24 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { HeaderComponent } from "./components/template/header/header.component";
 
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { HomeComponent } from './components/template/home/home.component';
+import { CadastroComponent } from './components/template/cadastro/cadastro.component';
+import { AdicionarComponent } from './components/template/adicionar/adicionar.component';
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, HeaderComponent, HomeComponent, CadastroComponent, AdicionarComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
